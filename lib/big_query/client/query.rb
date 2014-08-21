4c37b949867f181273a646fb0ac0d34ff3514b98
David Salgado <david@digitalronin.com>
2014-08-21 11:56:32 +0100

    query and each_row take a hash instead of a query string
    Default timeout to 90s, maxResults to 10,000

--- lib/big_query/client/query.rb
+++ lib/big_query/client/query.rb
@@ -3,46 +3,63 @@ module BigQuery
     module Query
       # Performs the given query in the bigquery api
       #
-      # @param given_query [String] query to perform
       # @param options [Hash] query options
-      # @option options [Integer] timeout (90 * 1000) timeout in miliseconds
+      # @option options [Integer] timeoutMs (90_000) timeout in miliseconds
+      # @option options [Integer] maxResults (10_000) max results to fetch per page
       # @return [Hash] json api response
-      def query(given_query, options={})
-        timeout = options.fetch(:timeout, 90 * 1000)
-        response = api(
+      def query(options)
+        params = body_params(options)
+        params.fetch(:query) # blow up if query not given
+        api(
           api_method: @bq.jobs.query,
-          body_object: { 'query' => given_query,
-                         'timeoutMs' => timeout}
+          body_object: params
         )
-
-        response
       end
 
       # perform a query synchronously
       # fetch all result rows, even when that takes >1 query
       # invoke /block/ once for each row, passing the row
       #
-      # @param q [String] query to be executed
       # @param options [Hash] query options
-      # @option options [Integer] timeout (90 * 1000) timeout in miliseconds
-      def each_row(q, options = {}, &block)
+      # @option options [Integer] timeoutMs (90_000) timeout in miliseconds
+      # @option options [Integer] maxResults (10_000) max results to fetch per page
+      def each_row(options, &block)
         current_row = 0
         # repeatedly fetch results, starting from current_row
         # invoke the block on each one, then grab next page if there is one
         # it'll terminate when res has no 'rows' key or when we've done enough rows
         # perform query...
-        res = query(q, options)
+        res = query(options)
         job_id = res['jobReference']['jobId']
         # call the block on the first page of results
         if( res && res['rows'] )
           res['rows'].each(&block)
           current_row += res['rows'].size
         end
+
         # keep grabbing pages from the API and calling the block on each row
-        while(( res = get_query_results(job_id, :startIndex => current_row) ) && res['rows'] && current_row < res['totalRows'].to_i ) do
+        while(( res = get_query_results(job_id, page_params(current_row, options)) ) && res['rows'] && current_row < res['totalRows'].to_i ) do
           res['rows'].each(&block)
           current_row += res['rows'].size
         end
+      end
+
+      private
+
+      def body_params(options)
+        {
+          timeoutMs:  90_000,
+          maxResults: 10_000
+        }.merge(options)
+      end
+
+      def page_params(current_row, options)
+        body = body_params(options)
+        {
+          startIndex: current_row,
+          timeoutMs:  body.fetch(:timeoutMs),
+          maxResults: body.fetch(:maxResults)
+        }
       end
     end
   end
